08df849ac9e34bb5b342c9f38073d881ff2af749
DeKaN <dekanszn@gmail.com>
2014-01-05 22:46:34 +0400

    Added test for getting records

--- UnitTestProject/UnitTest.cs
+++ UnitTestProject/UnitTest.cs
@@ -11,16 +11,40 @@
     [TestClass]
     public class UnitTest
     {
+        private const int maxDiff = 10, count = 5;
+
+        private const MessageLevel level = MessageLevel.Error;
+
+        private DateTime baseDate = DateTime.Now;
+        Random rg = new Random();
+
+        private ClientMessage GenerateMessage()
+        {
+            return new ClientMessage { Date = baseDate.AddDays(rg.Next(-maxDiff, maxDiff + 1)), Level = level };
+        }
 
         [TestMethod]
         public void TestAdd()
         {
-            FakeDataManager manager = new FakeDataManager();
-            ClientMessage msg = new ClientMessage { Date = DateTime.Now, Level = MessageLevel.Error };
-            LogService service = new LogService(manager);
+            var manager = new FakeDataManager();
+            var msg = GenerateMessage();
+            var service = new LogService(manager);
             service.AddRecord(msg);
             var found = manager.Messages.Where(m => m.Date.Equals(msg.Date)).ToList();
             Assert.AreEqual(1, found.Count);
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            var manager = new FakeDataManager();
+            for (int i = 0; i < count; i++)
+            {
+                manager.Messages.Add(GenerateMessage());
+            }
+            var service = new LogService(manager);
+            var loaded = service.GetMessages(level, baseDate.AddDays(-maxDiff), baseDate.AddDays(maxDiff));
+            Assert.AreEqual(count, loaded.Count);
         }
     }
 }
